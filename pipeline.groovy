f9c6cc5d08f9bdcce187e09c20928eda744b1c4f
ankitd1997 <63224222+ankitd1997@users.noreply.github.com>
2025-08-31 00:03:26 +0530

    Update pipeline.groovy

--- pipeline.groovy
+++ pipeline.groovy
@@ -16,7 +16,7 @@ pipeline {
         }
         stage('code-deploy'){
             steps {
-                withCredentials([aws(accessKeyVariable: 'AWS_ACCESS_KEY_ID', credentialsId: 'aws-creds', secretKeyVariable: 'AWS_SECRET_ACCESS_KEY')]) {
+                withCredentials([aws(accessKeyVariable: 'AWS_ACCESS_KEY_ID', credentialsId: 'ankit', secretKeyVariable: 'AWS_SECRET_ACCESS_KEY')]) {
                   sh '''
                       aws s3 cp --recursive dist/angular-frontend s3://cbz-frontend-project-bux123-king-mouse-new/
                  '''
